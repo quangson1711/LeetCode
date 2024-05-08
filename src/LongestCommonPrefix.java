3a14e43db0d163fc827274c6b20342c9b15c9375
sonso113 <luuquang.son@epayjsc.com>
2024-05-08 15:01:25 +0700

    Longest Common Prefix

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestCommonPrefix {
+    /*    Example 1:
+
+        Input: strs = ["flower","flow","flight"]
+        Output: "fl"
+        Example 2:
+
+        Input: strs = ["dog","racecar","car"]
+        Output: ""
+        Explanation: There is no common prefix among the input strings.*/
+    public static String longestCommonPrefix(String[] strs) {
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+
+                if ( i == strs[j].length()  || strs[0].charAt(i) != strs[j].charAt(i)){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+            return strs[0];
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"ab", "a"};
+        System.out.println(longestCommonPrefix(strings));
+    }
+}
