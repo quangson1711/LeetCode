7b4a7998a3f207b6e8085d6464e75f49cbf741c9
sonso113 <luuquang.son@epayjsc.com>
2024-05-09 11:17:59 +0700

    Valid Parentheses

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,29 +1,50 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ValidParentheses {
 
-/*    Example 1:
+    /*    Example 1:
 
-    Input: s = "()"
-    Output: true
-    Example 2:
+        Input: s = "()"
+        Output: true
+        Example 2:
 
-    Input: s = "()[]{}"
-    Output: true
-    Example 3:
+        Input: s = "()[]{}"
+        Output: true
+        Example 3:
 
-    Input: s = "(]"
-    Output: false*/
-    public boolean isValid(String s) {
-        Map<Character, Character> map = new HashMap<>();
-        map.put('(', '(');
-        map.put(')', ')');
-        map.put('[', '[');
-        map.put(']', ']');
-        map.put('{', '{');
-        map.put('}', '}');
+        Input: s = "(]"
+        Output: false*/
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++){
+            char value = s.charAt(i);
+            if (value == '(' || value == '[' || value == '{') {
+                if (value == '('){
+                    stack.push(')');
+                } else if (value == '['){
+                    stack.push(']');
+                } else {
+                    stack.push('}');
+                }
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char valueHead = stack.pop();
+                if (valueHead != value){
+                    return false;
+                }
+            }
+        }
 
-        for (int )
+        if (!stack.isEmpty()){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("]"));
     }
 }
